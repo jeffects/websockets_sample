04dcc79e76ded54f1be4167ffe59f678694a933d
Jeffrey Leung <jeffreyleung@alumni.calpoly.edu>
2013-06-23 23:39:44 -0700

    added rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebsocketsSample::Application.config.secret_key_base = 'd36f1720de5c7976e1fa6c244576cc4382e735dbe381ef79ac2a78dc79bbc9c14e43ccb6f09634a8b208970eb8c99d695d116e9673acc178e7703ce1a9224002'
